03750843ad69964e61583cc853e6d5454af5f346
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2016-05-29 12:23:31 +0200

    add twig loader

---
+++ src/Loader/Twig.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Twig Template Loader
+ *
+ * Twig template loader loads and parses the twig template files.
+ *
+ * @package   SlaxWeb\ViewTwig
+ * @author    Tomaz Lovrec <tomaz.lovrec@gmail.com>
+ * @copyright 2016 (c) Tomaz Lovrec
+ * @license   MIT <https://opensource.org/licenses/MIT>
+ * @link      https://github.com/slaxweb/
+ * @version   0.4
+ */
+namespace SlaxWeb\ViewTwig\Loader;
+
+use Twig_Environment;
+use SlaxWeb\View\AbstractLoader;
+
+class Twig extends \SlaxWeb\View\AbstractLoader
+{
+    /**
+     * Template file extension
+     *
+     * @var string
+     */
+    protected $_fileExt = "html";
+
+    /**
+     * Twig
+     *
+     * @var \Twig_Environment
+     */
+    protected $_twig = null;
+
+    /**
+     * Class constructor
+     *
+     * Assigns the dependant Response object to the class property. The View loader
+     * will automatically add template contents to as response body.
+     *
+     * @param \Symfony\Component\HttpFoundation\Response $response Response object
+     * @param \Psr\Log\LoggerInterface $logger PSR4 compatible Logger object
+     * @param \Twig_Environment $twig Twig template loader
+     * @return void
+     */
+    public function __construct(Response $response, Logger $logger, Twig_Environment $twig)
+    {
+        $this->_twig = $twig;
+
+        parent::__construct($response, $logger);
+    }
+
+    /**
+     * Load template
+     *
+     * Load and render the template file with twig.
+     *
+     * @param string $template Path to the template file
+     * @param array $data View data
+     * @return string
+     */
+    protected function _load(string $template, array $data): string
+    {
+        return $this->_twig->render($template, $data);
+    }
+}
